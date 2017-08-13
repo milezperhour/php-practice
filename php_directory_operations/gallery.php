d3b7e33398b237e8f7791ad94e9cb952e3081c1f
milezperhour <hyperbolvoid@gmail.com>
2017-08-13 11:00:22 -0700

    modal working

--- php_directory_operations/gallery.php
+++ php_directory_operations/gallery.php
@@ -3,10 +3,9 @@
     <head>
         <meta charset="utf-8">
         <title>PHP Directory Operations</title>
-        <?php
-
-         ?>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <script>
         $(document).ready(function(){
             load_files();
@@ -41,13 +40,48 @@
 
         function applyClickHandler() {
             $('img').click(function(){
-                
+                console.log('image was clicked');
+                $('img').attr({
+                    "data-toggle":"modal",
+                    "data-target":"#myModal"
+                });
+
+                var modalBody = $('.modal-body');
+                $(this).clone().appendTo(modalBody);
+                modalBody.children().css('width', '100%');
+            });
+            closeButtonClicked();
+        }
+
+        function closeButtonClicked() {
+            $('#closeButton').click(function(){
+                var modalBody = $('.modal-body');
+                modalBody.children().remove();
             });
         }
 
         </script>
     </head>
     <body>
+        <!-- Modal -->
+          <div class="modal fade" id="myModal" role="dialog">
+            <div class="modal-dialog">
 
+              <!-- Modal content-->
+              <div class="modal-content">
+                <div class="modal-header">
+                  <button type="button" class="close" data-dismiss="modal">&times;</button>
+                  <h4 class="modal-title">Modal Header</h4>
+                </div>
+                <div class="modal-body">
+                  <!--<img src="images/beagle_.jpg" width="100%"/>-->
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-default" id="closeButton" data-dismiss="modal">Close</button>
+                </div>
+              </div>
+
+            </div>
+          </div>
     </body>
 </html>
